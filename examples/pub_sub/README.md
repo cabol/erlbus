9e00187849f2277430f88a99ae62083c836e869a
cabol <candres.bolanos@gmail.com>
2015-06-16 14:41:51 -0500

    Added pub_sub example. Fixed build files, updated erlang.mk.

---
+++ examples/pub_sub/README.md
@@ -0,0 +1,16 @@
+Pub/Sub example
+===============
+
+To try this example, you need GNU `make` and `git` in your PATH.
+
+To build the example, run the following command:
+
+``` bash
+$ make
+```
+
+To start the example:
+
+``` bash
+$ make run
+```
---
+++ examples/pub_sub/src/pub_sub.erl
@@ -0,0 +1,46 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+-module(pub_sub).
+
+-behaviour(application).
+
+%% API.
+-export([start/0, start/2]).
+-export([stop/0, stop/1]).
+
+-define(HANDLER, pub_sub_handler).
+-define(TOPIC, pub_sub_topic).
+
+%% API.
+
+start() ->
+	application:ensure_all_started(pub_sub).
+
+start(_Type, _Args) ->
+	P = spawn_link(fun () -> publisher(?TOPIC) end),
+	lists:foreach(fun(N) -> subscriber(?TOPIC, N) end, lists:seq(1, 3)),
+	timer:sleep(1*60*1000),
+	exit(P, kill),
+	teardown_ebus().
+
+stop() ->
+	application:stop(pub_sub).
+
+stop(_State) ->
+	ok.
+
+%% Internals
+
+publisher(Topic) ->
+	timer:sleep(500),
+	Payload = now(),
+	io:format("Publish: ~p~n", [Payload]),
+	ebus:pub(Topic, Payload),
+	publisher(Topic).
+
+subscriber(Topic, N) ->
+	Handler = ebus_handler:new(?HANDLER, N),
+	ebus:sub(Topic, Handler).
+
+teardown_ebus() ->
+	application:stop(ebus).

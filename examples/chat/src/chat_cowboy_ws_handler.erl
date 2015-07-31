c142946bc80df564ec2c811490d07533bc0574c1
ferigis <ferigis@gmail.com>
2015-07-30 19:37:29 -0600

    Chat Example : Switching to cowboy 1.0 in order to support previous Erlang versions

--- examples/chat/src/chat_cowboy_ws_handler.erl
+++ examples/chat/src/chat_cowboy_ws_handler.erl
@@ -1,9 +1,10 @@
 -module(chat_cowboy_ws_handler).
 
--export([init/2]).
+-export([init/3]).
+-export([websocket_init/3]).
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
--export([terminate/3]).
+-export([websocket_terminate/3]).
 
 -define(CHATROOM_NAME, ?MODULE).
 -define(TIMEOUT, 5 * 60 * 1000). % Innactivity Timeout
@@ -12,11 +13,14 @@
 
 %% API
 
-init(Req, _Opts) ->
+init(_, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_websocket}.
+
+websocket_init(_Type, Req, _Opts) ->
   % Create the handler from our custom handler
   Handler = ebus_handler:new(chat_erlbus_handler, self()),
   ebus:sub(?CHATROOM_NAME, Handler),
-  {cowboy_websocket, Req, #state{name = get_name(Req), handler = Handler}, ?TIMEOUT}.
+  {ok, Req, #state{name = get_name(Req), handler = Handler}, ?TIMEOUT}.
 
 websocket_handle({text, Msg}, Req, State) ->
   ebus:pub(?CHATROOM_NAME, {State#state.name, Msg}),
@@ -29,7 +33,7 @@ websocket_info({message_published, {Sender, Msg}}, Req, State) ->
 websocket_info(_Info, Req, State) ->
   {ok, Req, State}.
 
-terminate(_Reason, _Req, State) ->
+websocket_terminate(_Reason, _Req, State) ->
   % Unsubscribe the handler
   ebus:unsub(?CHATROOM_NAME, State#state.handler),
   ok.
@@ -37,7 +41,7 @@ terminate(_Reason, _Req, State) ->
 %% Private methods
 
 get_name(Req) ->
-  {Host, Port} = cowboy_req:peer(Req),
+  {{Host, Port}, _} = cowboy_req:peer(Req),
   Name = list_to_binary(string:join([inet_parse:ntoa(Host), 
     ":", io_lib:format("~p", [Port])], "")),
   Name.

5663d2a305202eae3fead1492e901c1563ae5770
Felipe Ripoll <ferigis@gmail.com>
2015-07-30 18:18:56 -0600

    Chat Example : Unsubscribing from the channel when terminate

--- examples/chat/src/chat_cowboy_ws_handler.erl
+++ examples/chat/src/chat_cowboy_ws_handler.erl
@@ -8,7 +8,7 @@
 -define(CHATROOM_NAME, ?MODULE).
 -define(TIMEOUT, 5 * 60 * 1000). % Innactivity Timeout
 
--record(state, {name}).
+-record(state, {name, handler}).
 
 %% API
 
@@ -16,7 +16,7 @@ init(Req, _Opts) ->
   % Create the handler from our custom handler
   Handler = ebus_handler:new(chat_erlbus_handler, self()),
   ebus:sub(?CHATROOM_NAME, Handler),
-  {cowboy_websocket, Req, #state{name=get_name(Req)}, ?TIMEOUT}.
+  {cowboy_websocket, Req, #state{name = get_name(Req), handler = Handler}, ?TIMEOUT}.
 
 websocket_handle({text, Msg}, Req, State) ->
   ebus:pub(?CHATROOM_NAME, {State#state.name, Msg}),
@@ -29,7 +29,9 @@ websocket_info({message_published, {Sender, Msg}}, Req, State) ->
 websocket_info(_Info, Req, State) ->
   {ok, Req, State}.
 
-terminate(_Reason, _Req, _State) ->
+terminate(_Reason, _Req, State) ->
+  % Unsubscribe the handler
+  ebus:unsub(?CHATROOM_NAME, State#state.handler),
   ok.
 
 %% Private methods
